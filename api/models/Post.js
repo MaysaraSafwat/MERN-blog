8425369ef86c6d4bf7054a15eefec8eaabe3c3ca
Maysara Safwat <maisarasafwat2@gmail.com>
2021-09-08 19:30:22 +0200

    setup server & make routes

---
+++ api/models/Post.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+
+
+const PostSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : true,
+        unique : true
+
+    },
+    description : {
+        type : String,
+        required : true,
+    },
+    postPic : {
+        type : String,
+        required : false
+    },
+    username :{
+        type : String,
+        required : true,
+        unique : true
+    },
+    categories: {
+        type: Array,
+        required :false
+    }
+    
+}, {timestamps : true}
+);
+
+module.exports = mongoose.model("Post", PostSchema);
